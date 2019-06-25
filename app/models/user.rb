10d29e7704dcc41e6ebb34b76ed561d390cc4e7e
Grzegorz Fula <grzegorz.fula@gmail.com>
2019-06-25 23:18:11 +0200

    add validation to model, add seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ActiveRecord::Base
   has_many :book_histories
   before_save { self.email = email.downcase } 
   validates :name, 
-            :presence => true
+            :presence => {:message => "Wpisz imię"},
+            :length => {:maximum => 255}
   validates :email,
-            :presence => true,
+            :presence => {:message => "Wpisz email"},
             :uniqueness => {:message => "Email już istnieje"},
-            :email_format => {:message => "Email jest nie prawidłowy"}
-  validates :password,
-            :length => {:minimum => 8 },
-            :presence => {:message => "Wpisz hasło"},
-            :allow_nil => true
+            :email_format => {:message => "Email jest nie prawidłowy"},
+            :length => {:maximum => 255}
+  validates :address,
+            :length => {:maximum => 255}
+  validates :phone,
+            :length => {:minimum => 9, :maximum => 9},
+            :numericality => {:only_integer => true }
   has_secure_password
 end
